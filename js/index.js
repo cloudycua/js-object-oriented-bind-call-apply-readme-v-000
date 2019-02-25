aa2dd7c1d07ceb79da5bb4713bbe99d7ad2bb572
Christine Cua <cloudycua@gmail.com>
2019-02-25 14:18:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
-function greet() {
+function greet(customerOne, customerTwo) {
   console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}, hi!`)
 }
 
 greet();
-
-let person = {
-  name: 'Bob',
-  greet: greet
-};
 
 person.greet();
 
