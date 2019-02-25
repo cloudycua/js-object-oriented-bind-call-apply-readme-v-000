9a1cf6a842a6b40a3218d9dcfae7f79be478b5bd
Christine Cua <cloudycua@gmail.com>
2019-02-25 14:36:14 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -49,7 +49,7 @@ class User {
     return event.keywords.some(
       function(word) {
         return this.interests.includes(word);
-      }.bind(this)    // added to the end of the callback function
+      }.bind(this)    // added to the end of the callback function, refers to User this
     );
   }
 }
